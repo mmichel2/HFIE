aa3ce3a64343f054f4bb760b01d5d85de913658c
mmichel2 <70848005+mmichel2@users.noreply.github.com>
2021-01-06 16:06:34 +0100

    Code_erstellt

---
+++ Nim_Game_Michel/Nim_Game_Michel/Nim_Game_Michel.c
@@ -0,0 +1,10 @@
+#include <stdio.h>      // rpintf, scanf
+#include <stdint.h>     // int32_t
+#include <stdlib.h>     // rand()
+#include <time.h>       // initialisierung des Zufallsgenerators
+
+
+int main()
+{
+
+}
