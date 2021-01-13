a2fba10a9c9296feec19d23b1db1d1d47646d19b
mmichel2 <70848005+mmichel2@users.noreply.github.com>
2021-01-13 21:57:40 +0100

    update c-programm
    Funktion EingabeZahl
    Funktion Spielfeldausgabe

--- Nim_Game_Michel/Nim_Game_Michel/Nim_Game_Michel.c
+++ Nim_Game_Michel/Nim_Game_Michel/Nim_Game_Michel.c
@@ -3,7 +3,50 @@
 #include <stdlib.h>     // rand()
 #include <time.h>       // initialisierung des Zufallsgenerators
 
+
+void Spielfeldausgabe(int32_t daten_nim[]);
+int32_t EingabeZahl();
+// void Spielfeldgenerator(const int8_t hoelzchen, const int8_t reihen, );
+
+
+
 int main()
 {
-	//Komentar Test
+
+
 }
+
+
+int32_t EingabeZahl()
+{
+	int32_t eingabe = 0;
+	scanf_s("%i", &eingabe);
+	while (getchar() != '\n');
+	return eingabe;
+}
+
+void Spielfeldausgabe(int32_t daten_nim[])
+{
+	int32_t i = 0;
+	int32_t zaeler;
+	printf("Spielfeld\n\n");
+	while(i < sizeof(daten_nim))
+	{
+		zaeler = daten_nim[i];
+		if (daten_nim[i] != 0)
+		{
+			while (zaeler != 0) 
+			{
+				printf("| ");
+				zaeler--;
+			}
+		}
+		printf("\n___________________\n");
+		i++;
+	}
+}
+
+
+
+
+
